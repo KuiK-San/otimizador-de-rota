469a213b09b7a263ff4d6e694c295aac3a6db5f7
Guilherme Casagrande <thegui4000@gmail.com>
2023-10-23 16:56:52 -0300

    feat: pegando coordenadas do address

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
 import random
 import colorama
+import requests
 
 def calcular_distancia_geografica(lat1, lat2, lon1, lon2):
 
@@ -21,35 +22,48 @@ def calcular_distancia_geografica(lat1, lat2, lon1, lon2):
 
     return distancia
 
-points = [(-49.25391333333334, -25.508744999999998),(-49.24901333333334, -25.515295),(-49.240494999999996, -25.51330666666667),(-49.23087833333334, -25.51711833333333),(-49.25783499999999, -25.50466833333333),( -49.2101635, -25.5415646),(-49.2103243, -25.536708),(-49.255829999999996, -25.505578333333332)]
 
-# random.shuffle(points)
+def calcularRota(points, startPoint):
 
-startPoint = (-49.2101635, -25.5415646)
+    url = f'https://www.google.com/maps/dir'
+    for point, j in enumerate(points):
+        url = f'{url}/{points[point][1]},{points[point][0]}'
+    print(colorama.Fore.RED + str(url) + colorama.Fore.RESET)
 
-url = f'https://www.google.com/maps/dir'
-for point, j in enumerate(points):
-    url = f'{url}/{points[point][1]},{points[point][0]}'
-print(colorama.Fore.RED + str(url) + colorama.Fore.RESET)
+    route = []
+    i = 0
+    while len(points) != 0:
+        route.append(startPoint)
+        smallDistance = 9999999999
+        for j, point in enumerate(points):
+            distance = calcular_distancia_geografica(startPoint[1], point[1], startPoint[0], point[0])
+            if distance < smallDistance:
+                smallDistance = distance
+                newPoint = j
+        
+        
+        startPoint = points[newPoint]
+        points.pop(newPoint)
+        i+=1
 
-route = []
-i = 0
-while len(points) != 0:
-    route.append(startPoint)
-    smallDistance = 9999999999
-    for j, point in enumerate(points):
-        distance = calcular_distancia_geografica(startPoint[1], point[1], startPoint[0], point[0])
-        if distance < smallDistance:
-            smallDistance = distance
-            newPoint = j
-    
-    
-    startPoint = points[newPoint]
-    points.pop(newPoint)
-    i+=1
+    url = f'https://www.google.com/maps/dir'
+    for point, j in enumerate(route):
+        url = f'{url}/{route[point][1]},{route[point][0]}'
+    print(colorama.Fore.CYAN + str(url) + colorama.Fore.RESET)
 
-url = f'https://www.google.com/maps/dir'
-for point, j in enumerate(route):
-    url = f'{url}/{route[point][1]},{route[point][0]}'
-print(colorama.Fore.CYAN + str(url) + colorama.Fore.RESET)
 
+if __name__ == "__main__":
+    repetir = True
+    while repetir:
+        startPoint = 'Rua Cel. José Leal Fontoura 463, curitiba Xaxim'
+        url = f'https://us1.locationiq.com/v1/search?key=pk.8f71e14e24927150bc7d607a0c4b0c49&q={startPoint}&format=json'
+        data = {
+            'key': 'pk.8f71e14e24927150bc7d607a0c4b0c49',
+            'q': str(startPoint),
+            'format': 'json'
+        }
+        response = requests.get(url=url)
+        
+        if response.status_code == 200:
+            print(response.json())
+        
