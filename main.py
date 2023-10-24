f244af377505377648f6bd1782a43c5da2a26ab9
Guilherme Casagrande <thegui4000@gmail.com>
2023-10-24 15:15:05 -0300

    fix: Arrumandos alguns bugs

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import main
+import calculador
 import colorama
 
-def buscarCep(add):
+def burcarCoord(add):
     options = webdriver.ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
     options.add_argument('--headless')
@@ -27,21 +27,39 @@ if __name__ == '__main__':
     points = []
 
     startPoint = input('Digite o ponto inicial: ')
-    startPoint = buscarCep(startPoint)
+    startPoint = startPoint.split(' ')
+    if 'Endereço:' in startPoint:
+        startPoint.pop(0)
+
+    end = ''
+
+    for j, word in enumerate(startPoint):
+
+        if j == 0:
+            end = word
+        else:
+            end += ' ' + word
+    startPoint = end
+    startPoint = burcarCoord(startPoint)
 
     while repetir:
-        point = input(f"Qual o ponto {i}?\n")
-        point = buscarCep(point)
+        point = input(f"\nQual o ponto {i}? [Presione enter sem escrever nada para finalizar a coleta de pontos]\n")
+        if point == '':
+            break
+        point = point.split(' ')
+        if 'Endereço:' in point:
+            point.pop(0)
+        end = ''
+        for j, word in enumerate(point):
+            if j == 0:
+                end = word
+            else:
+                end += ' ' + word
+        point = end
+        point = burcarCoord(point)
         points.append(point)
-
-        continuar = input('Deseja inserir mais um ponto? [1] SIM [0] NÃO\n')
-        i = i + 1
-        if continuar == '0':
-            repetir = False
-        if continuar != '1':
-            while continuar != '1':
-                continuar = input('Deseja inserir mais um ponto? [1] SIM [0] NÃO\n')
+        i += 1
 
     
-    url = main.calcularRota(points, startPoint)
+    url = calculador.calcularRota(points, startPoint)
     print(colorama.Fore.CYAN + str(url) + colorama.Fore.RESET)
