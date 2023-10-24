198b3d5b31cd6b55a7a7ca00b1504d8a7650ace7
Guilherme Casagrande <thegui4000@gmail.com>
2023-10-24 14:09:51 -0300

    feat: funcionando 100% preciso

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
 import random
-import colorama
 import requests
 
 def calcular_distancia_geografica(lat1, lat2, lon1, lon2):
@@ -28,7 +27,6 @@ def calcularRota(points, startPoint):
     url = f'https://www.google.com/maps/dir'
     for point, j in enumerate(points):
         url = f'{url}/{points[point][0]},{points[point][1]}'
-    print(colorama.Fore.RED + str(url) + colorama.Fore.RESET)
 
     route = []
     i = 0
@@ -49,57 +47,6 @@ def calcularRota(points, startPoint):
     url = f'https://www.google.com/maps/dir'
     for point, j in enumerate(route):
         url = f'{url}/{route[point][0]},{route[point][1]}'
-    print(colorama.Fore.CYAN + str(url) + colorama.Fore.RESET)
-
-
-if __name__ == "__main__":
-    repetir = True
-    while repetir:
-        startPoint = input('Insira o endereço inicial:\n')
-        url = f'https://us1.locationiq.com/v1/search?key=pk.8f71e14e24927150bc7d607a0c4b0c49&q={startPoint}&format=json'
-        response = requests.get(url=url)
-        
-        if response.status_code == 200:
-            # print(response.json())
-            resposta = response.json()
-            escolha = input(f"{resposta[0]['display_name'].split('Região')[0]}é o endereço correto? [1] SIM [0] NÃO\n")
-            if len(escolha) == 1:
-                if int(escolha) == 1:
-                    startPoint = [resposta[0]['lat'],resposta[0]['lon']]
-                    repetir = False
-            else:
-                print('invalido')
-        else:
-            print('Falha na procura...')
-
-    repetir = True
-    i = 1
-    points = []
-    while repetir:
-        point = input(f'Insira o {i} ponto:\n')
-        url = f'https://us1.locationiq.com/v1/search?key=pk.8f71e14e24927150bc7d607a0c4b0c49&q={point}&format=json'
-        response = requests.get(url=url)
-
-        if response.status_code == 200:
-            # print(response.json())
-            resposta = response.json()
-            escolha = input(f"{resposta[0]['display_name'].split('Região')[0]}é o endereço correto? [1] SIM [0] NÃO\n")
-            if len(escolha) == 1:
-                if int(escolha) == 1:
-                    points.append([resposta[0]['lat'],resposta[0]['lon']])
-                    escolha = input(f"Deseja adicionar mais um ponto? [1] SIM [0] NÃO\n")
-                    i += 1
-                    if len(escolha) == 1:
-                        if int(escolha) == 0:
-                            repetir = False
-                    else:
-                        print('Invalido')
-
-            else:
-                print('Invalido')
-        else:
-            print('Falha na procura...')
     
-    print('Criando melhor rota...')
+    return url
 
-    calcularRota(points, startPoint)
